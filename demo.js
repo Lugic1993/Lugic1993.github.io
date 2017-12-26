ac9af5fca7a6b26c52607da44feb5dc2857333fa
lugic <tdstevelx333@outlook.com>
2017-12-26 18:01:35 +0600

    no.1

---
+++ demo.js
@@ -0,0 +1,61 @@
+
+var box = 'Mr.Jason As 2017 slowly winds to a close, you may have noticed the sun beginning to set earlier and earlier, and you’re right! As the seasons begin to change, so too does the earth’s allocation of daylight.December 21st marks the 2017 winter solstice, as well as the first day of winter on the astronomical calendar. On this day, the tilt of the earth’s North Pole is at its farthest from the sun, resulting in the shortest day and the longest night of the calendar year.Though most refer to the solstice as an entire day, in reality, the solstice is defined as a single moment: when the sun is directly above the Tropic of Capricorn. This year, that moment will occur at 16:28 Coordinated Universal Time (UTC). After this point in time, periods of daylight will once again begin to grow longer.'
+
+/*
+var boxarr = [];
+var pos = box.indexOf('the');
+while (pos > -1){
+	boxarr.push(pos);
+	pos = box.indexOf('the', pos +1);
+};
+alert(boxarr);
+alert(boxarr.length);
+
+
+var boxarr = [];
+boxarr = box.split(' ');
+alert(boxarr.length);
+
+var box2 = box.toUpperCase();
+document.write(box2);
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
