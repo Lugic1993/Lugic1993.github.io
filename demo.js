6b91c5a1f24df03140fbcc96b099ea80401d0102
lugic <tdstevelx333@outlook.com>
2017-12-26 19:30:10 +0600

    改名

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 
-var box = 'Mr.Jason As 2017 slowly winds to a close, you may have noticed the sun beginning to set earlier and earlier, and you’re right! As the seasons begin to change, so too does the earth’s allocation of daylight.December 21st marks the 2017 winter solstice, as well as the first day of winter on the astronomical calendar. On this day, the tilt of the earth’s North Pole is at its farthest from the sun, resulting in the shortest day and the longest night of the calendar year.Though most refer to the solstice as an entire day, in reality, the solstice is defined as a single moment: when the sun is directly above the Tropic of Capricorn. This year, that moment will occur at 16:28 Coordinated Universal Time (UTC). After this point in time, periods of daylight will once again begin to grow longer.'
+//var box = 'Mr.Jason As 2017 slowly winds to a close, you may have noticed the sun beginning to set earlier and earlier, and you’re right! As the seasons begin to change, so too does the earth’s allocation of daylight.December 21st marks the 2017 winter solstice, as well as the first day of winter on the astronomical calendar. On this day, the tilt of the earth’s North Pole is at its farthest from the sun, resulting in the shortest day and the longest night of the calendar year.Though most refer to the solstice as an entire day, in reality, the solstice is defined as a single moment: when the sun is directly above the Tropic of Capricorn. This year, that moment will occur at 16:28 Coordinated Universal Time (UTC). After this point in time, periods of daylight will once again begin to grow longer.'
 
 /*
 var boxarr = [];
@@ -21,18 +21,65 @@ document.write(box2);
 
 
 
+
+
+var num = 0;
+var max = 5;
+var bbb = null;
+
+function box() {
+	num++;
+	document.getElementById('b').innerHTML += num;
+	if (num == max) {
+		clearInterval(bbb);
+		setTimeout(function() {
+			alert("5秒到了");
+		}, 10);
+	}
+};
+
+bbb = setInterval(box, 1000);
+
+
+
+var num = 0;
+var max = 5;
+var bbb = null;
+
+function box() {
+	num++;
+	document.getElementById('b').innerHTML += num;
+	if (num == max) {
+		setTimeout(function() {
+			alert("5秒到了");
+		}, 10);
+	} else {
+		setTimeout(box, 1000);
+	}
+};
+
+setTimeout(box, 1000);
+
+
 */
 
+var num = 0;
+var max = 5;
+var bbb = null;
 
+function box() {
+	num++;
+	document.getElementById('b').innerHTML = num;
+	if (num == max) {
+		setTimeout(function() {
+			alert("5秒到了");
+		}, 10);
+	} else {
+		setTimeout(box, 1000);
+	}
+};
 
-
-
-
-
-
-
-
-
+setTimeout(box, 1000);
 
 
 
